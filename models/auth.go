c5c745e16ebccb0c9d2d22134da278c96f2c3f9b
Kerlin Huang <ikerlin@msn.com>
2018-10-31 20:24:43 +0800

    移除多余代码

--- models/auth.go
+++ models/auth.go
@@ -1,13 +1,8 @@
 package models
 
-import "fmt"
-
 func CheckAuth(username string, password string) bool {
 	user := User{}
 	user = user.GetByUsernameAndPassword(username, password)
-
-	fmt.Printf("%+v\n", user)
-	fmt.Printf("%s\n", user.CreatedAt)
 
 	if user.ID > 0 {
 		return true
