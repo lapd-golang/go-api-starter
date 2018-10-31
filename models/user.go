1391121aa2f0d2d66cf898cb96a47e23d2640607
Kerlin Huang <ikerlin@msn.com>
2018-10-31 20:32:32 +0800

    修正查询用户错误

--- models/user.go
+++ models/user.go
@@ -8,7 +8,7 @@ type User struct {
 }
 
 func (u *User) GetByUsernameAndPassword(username string, password string) (user User) {
-	db.Where(User{Username: u.Username, Password: u.Password}).First(&user)
+	db.Where(User{Username: username, Password: password}).First(&user)
 
 	return
 }
