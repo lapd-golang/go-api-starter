50e2e539d4963e5cbb49f20a6c0b047b047accd4
Kerlin Huang <ikerlin@msn.com>
2018-12-07 10:24:37 +0800

    调整响应

--- controllers/api/v1/role.go
+++ controllers/api/v1/role.go
@@ -1,15 +1,29 @@
 package v1
 
 import (
+	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"go-admin-starter/models"
-	"net/http"
+	"go-admin-starter/utils/app"
+	"go-admin-starter/utils/e"
 )
 
 func AddCasbin(c *gin.Context) {
 	rolename := c.PostForm("rolename")
 	path := c.PostForm("path")
 	method := c.PostForm("method")
+
+	valid := validation.Validation{}
+	valid.Required(rolename, "rolename").Message("角色名称不能为空")
+	valid.Required(path, "path").Message("路径不能为空")
+	valid.Required(method, "method").Message("请求方法不能为空")
+
+	if valid.HasErrors() {
+		app.MarkErrors(valid.Errors)
+		app.Response(c, e.INVALID_PARAMS, valid.Errors[0].Message, nil)
+		return
+	}
+
 	ptype := "p"
 	casbin := models.CasbinModel{
 		Ptype:    ptype,
@@ -19,14 +33,10 @@ func AddCasbin(c *gin.Context) {
 	}
 	isok := casbin.AddCasbin(casbin)
 	if isok {
-		c.JSON(http.StatusOK, gin.H{
-			"success": true,
-			"msg":     "保存成功",
-		})
+		app.Response(c, e.SUCCESS, "保存成功", nil)
+		return
 	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"success": false,
-			"msg":     "保存失败",
-		})
+		app.Response(c, e.ERROR, "保存失败", nil)
+		return
 	}
 }
--- middleware/authrole/authrole.go
+++ middleware/authrole/authrole.go
@@ -4,7 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"go-admin-starter/middleware/jwt"
 	"go-admin-starter/models"
-	"net/http"
+	"go-admin-starter/utils/app"
+	"go-admin-starter/utils/e"
 )
 
 //权限检查中间件
@@ -13,25 +14,17 @@ func AuthCheckRole() gin.HandlerFunc {
 		//根据上下文获取载荷claims 从claims获得role
 		claims := c.MustGet("claims").(*jwt.Customclaims)
 		role := claims.Role
-		e := models.Casbin()
+		enforcer := models.Casbin()
 		//检查权限
-		res, err := e.EnforceSafe(role, c.Request.URL.Path, c.Request.Method)
+		res, err := enforcer.EnforceSafe(role, c.Request.URL.Path, c.Request.Method)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"status": -1,
-				"msg":    "错误消息" + err.Error(),
-			})
-			c.Abort()
+			app.Response(c, e.ERROR_AUTH_ROLE, "检查权限错误: "+err.Error(), nil)
 			return
 		}
 		if res {
 			c.Next()
 		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"status": 0,
-				"msg":    "很抱歉您没有此权限",
-			})
-			c.Abort()
+			app.Response(c, e.ERROR_AUTH_ROLE, "很抱歉您没有此权限", nil)
 			return
 		}
 	}
